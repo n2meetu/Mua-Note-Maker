de8488abd543c98e471edcb00967a6beac64610c
Greta <zhangwen@ZhangWendeMacBook-Air.local>
2026-04-04 19:44:14 +0800

    feat: 添加 Vercel 部署配置2

--- miniprogram/utils/config.js
+++ miniprogram/utils/config.js
@@ -1,12 +1,6 @@
 // 配置文件
-// 请安装 ngrok:
-//   brew install ngrok
-//   ngrok http 3000
-//   复制 ngrok 给的 https 地址
-// 例如: https://abc123.ngrok.io
-//   修改下面的配置为 ngrok 地址
-const API_BASE_URL = 'https://cliffless-anastacia-overbookishly.ngrok-free.dev'
-// ⚠️ 手机浏览器测试: http://192.168.3.239:3000/health
+// 生产环境：Vercel 部署
+const API_BASE_URL = 'https://mua-note-maker.vercel.app'
 
 module.exports = {
   API_BASE_URL,
